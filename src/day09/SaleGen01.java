ba7534ec66c15b83c253affe1d5ef5defcd09f2d
userName <email>
2024-10-17 18:12:41 +0900

    day14

--- src/day09/SaleGen01.java
+++ src/day09/SaleGen01.java
@@ -1,14 +1,14 @@
 package day09;
 
 //Generics => <K, V> 대문자로 사용
-public class Sale<K,V> {
+public class SaleGen01<K,V> {
 	// 제너릭의 타입 값은 대문자로...
 	// 멤버변수 id, menum price
 	private K id;
 	private V menu;
 	private int price;
-	public Sale() {}
-	public Sale(K id, V menu, int price) {
+	public SaleGen01() {}
+	public SaleGen01(K id, V menu, int price) {
 		this.id = id;
 		this.menu = menu;
 		this.price = price;
